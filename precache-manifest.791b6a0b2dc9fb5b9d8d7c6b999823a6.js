d4560f8a5c7f311bd974802dd44ad9f690ec3d16
Edgar018 <Edgar_belandria010@hotmail.com>
2020-04-26 12:29:28 -0400

    Updates

---
+++ precache-manifest.791b6a0b2dc9fb5b9d8d7c6b999823a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e69b9c92a3abf8cccd3be1f164569f2",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "40695943fbbc62ecd4a2",
+    "url": "/Weather-App/static/css/main.ab066191.chunk.css"
+  },
+  {
+    "revision": "e4553b9f3b2a218f6e32",
+    "url": "/Weather-App/static/js/2.0f9f4886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.0f9f4886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40695943fbbc62ecd4a2",
+    "url": "/Weather-App/static/js/main.941ea71d.chunk.js"
+  },
+  {
+    "revision": "40f4fc8b2dd9803f5599",
+    "url": "/Weather-App/static/js/runtime-main.77c1eddf.js"
+  }
+]);
